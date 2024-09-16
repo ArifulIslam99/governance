88e6283bca3469c7fae0bc47fecc056cac877424
ArifulIslam99 <marifulislam0999@gmail.com>
2024-09-16 20:06:47 +0600

    Update the methods

--- Frontend/governance-frontend/src/Components/CreateProposal.jsx
+++ Frontend/governance-frontend/src/Components/CreateProposal.jsx
@@ -12,7 +12,7 @@ function CreateProposal() {
         const trx = new Transaction();
         
         trx.moveCall({
-            target: `0x5ba420c8f49ccf8589f42169a68039ff0370414b7c5fbd0a316eaf4ea73b9f27::governance::process_user_request`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::governance::process_user_request`,
             arguments: [
                 trx.pure.address('0xda4a99dc2473de94a1711b33cd18bbea7bf517ed432ea09c3e90e993ed6f5410'),
                 trx.pure.bool(true),
@@ -70,11 +70,35 @@ function CreateProposal() {
     const VoteUserRequest = () => {
         const trx = new Transaction();
         trx.moveCall({
-            target: `0x5ba420c8f49ccf8589f42169a68039ff0370414b7c5fbd0a316eaf4ea73b9f27::governance::vote_user_request`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::governance::vote_user_request`,
             arguments: [
                 trx.object('0xadb0fb8d0e715763a4359dee593ee268e6a5b113dea803b548c1c7b129df6f41'),
                 trx.pure.address('0xd6fe0d5ffb1a84110cfd1f28749de6082d9c93f98c4587b7f1b8100e98ccf444'),
                 trx.object('0xc4132aac7d00f91ab855a48f8a7b6ef72a441d8ba26240c3fc6a515f6eebb48c'),
+                trx.object('0x0000000000000000000000000000000000000000000000000000000000000006')
+            ]
+        });
+
+        signAndExecute(
+            {
+                transaction: trx,
+            },
+            {
+                onError: (err) => {
+                    console.log(err)
+                }
+            }
+        );
+    }
+
+    const VoteOnProposal = () => {
+        const trx = new Transaction();
+        trx.moveCall({
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::governance::vote_proposal`,
+            arguments: [
+                trx.object('0x510545dc37da1fb3459d79f260a457a2530df381f062b6d94ea8fc4c77cd3f05'),
+                trx.object('0x51c399f7cded75f987ab4340825a72615b5b83ed32dce794e86ca6ef20e34f83'),
+                trx.object('0xb8610294c2f92b9fa50d9253d86e05c3418d60bb4a06a1d3c1c074f15edbbdfd'),
                 trx.object('0x0000000000000000000000000000000000000000000000000000000000000006')
             ]
         });
@@ -99,7 +123,7 @@ function CreateProposal() {
         console.log("Checked")
         const trx = new Transaction();
         trx.moveCall({
-            target: `0x5ba420c8f49ccf8589f42169a68039ff0370414b7c5fbd0a316eaf4ea73b9f27::governance::decide_user_action`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::governance::decide_user_action`,
             arguments: [
                 trx.object('0xadb0fb8d0e715763a4359dee593ee268e6a5b113dea803b548c1c7b129df6f41'),
                 trx.pure.address('0xd6fe0d5ffb1a84110cfd1f28749de6082d9c93f98c4587b7f1b8100e98ccf444'),
@@ -122,13 +146,13 @@ function CreateProposal() {
     const submitNewProposal = () => {
         const trx = new Transaction();
         trx.moveCall({
-            target: `0x5ba420c8f49ccf8589f42169a68039ff0370414b7c5fbd0a316eaf4ea73b9f27::governance::create_proposal`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::governance::create_proposal`,
             arguments: [
-                trx.pure.string('New Voting Mechanism'),
-                trx.object('0xa5e8b306f2126f8c78d630d6c41c9c7510f0389fa735004eeba78451b9aa112b'),
-                trx.object('0x6d98f83c8bce3e6c0081ec264fb5f24301050e4e1f86e3a60088e44cbbd1232d'),
-                trx.pure.u64(250),
-                trx.object('0xc9950a30ee8a4315c924031a0651bb12ae1320345d2d44224da36247997b96ee'),
+                trx.pure.string('Increase Staking Rewards'),
+                trx.object('0x7bbeebd2669e05c52fd9cd00df219fc8c70ceaa8c2c327cc5264ecee866945c1'),
+                trx.object('0x510545dc37da1fb3459d79f260a457a2530df381f062b6d94ea8fc4c77cd3f05'),
+                trx.pure.u64(200),
+                trx.object('0xb8610294c2f92b9fa50d9253d86e05c3418d60bb4a06a1d3c1c074f15edbbdfd'),
             ]
         });
 
@@ -147,7 +171,7 @@ function CreateProposal() {
     const retrievePublicKey = async () => {
         const trx = new Transaction();
         trx.moveCall({
-            target: `0x9222f45aa00b2712c027b37bad31342fedb52de1d9aa049bca67e935c5e2e5dc::protocol::get_public_key`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::protocol::get_public_key`,
             arguments: [
                 trx.object('0xd82471a9da34b6d5bc0c2736ce05a268936ce5a6714d1e6e917a82817232ae44'),
                 trx.pure.string("Ariful Afridi"),
@@ -165,7 +189,7 @@ function CreateProposal() {
     const retrieveUsers  = async() => {
         const trx = new Transaction();
         trx.moveCall({
-            target: `0x9222f45aa00b2712c027b37bad31342fedb52de1d9aa049bca67e935c5e2e5dc::protocol::get_all_usernames`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::protocol::get_all_usernames`,
             arguments: [
                 trx.object('0xd82471a9da34b6d5bc0c2736ce05a268936ce5a6714d1e6e917a82817232ae44'),
             ]
@@ -182,7 +206,7 @@ function CreateProposal() {
     const retrieveBlobid = async () => {
         const trx = new Transaction();
         trx.moveCall({
-            target: `0x9222f45aa00b2712c027b37bad31342fedb52de1d9aa049bca67e935c5e2e5dc::protocol::get_blob_ids`,
+            target: `0x56a6f42c20f5422292c3ce30dd55746492e75360ec41ade0074290c765fe4bf9::protocol::get_blob_ids`,
             arguments: [
                 trx.object('0x478f4467244032f0d8c79eeaa72084aedc32b87f4efc28a979f6f7718f8fa481'),
                 trx.pure.string("abcwallet")
@@ -262,9 +286,9 @@ function CreateProposal() {
 
             <div>
             <button
-                    onClick={retrieve_proposals}
+                    onClick={VoteOnProposal}
                     className="btn btn-active btn-secondary"
-                >Get All Proposals</button>
+                >vote</button>
             </div>
 
         </div>
--- sources/governance.move
+++ sources/governance.move
@@ -32,7 +32,7 @@ module governance::governance {
 
     public struct ProposalList has key {
         id: UID,
-        list: Table<address, Proposal>,
+        list: Table<ID, Proposal>,
     }
 
     public struct Users has key, store {
@@ -42,7 +42,7 @@ module governance::governance {
 
     public struct UserRequestList has key {
         id: UID,
-        list: Table<address, HandleUser>
+        list: Table<ID, HandleUser>
     }
 
     public struct HandleUser has key, store {
@@ -108,10 +108,10 @@ module governance::governance {
             last_voting_time: 0,
             voter_list: table::new(ctx)
         };
-        table::add(&mut proposal_list.list, object::uid_to_address(&new_proposal.id), new_proposal);
+        table::add(&mut proposal_list.list, object::uid_to_inner(&new_proposal.id), new_proposal);
     }
 
-    public entry fun vote_proposal(proposal_list: &mut ProposalList, proposal: address, users: &Users, clock: &Clock, ctx: &mut TxContext) {
+    public entry fun vote_proposal(proposal_list: &mut ProposalList, proposal: ID, users: &Users, clock: &Clock, ctx: &mut TxContext) {
         assert!(table::contains(&users.list, tx_context::sender(ctx)), ENOTDAOMEMBER);
         assert!(table::contains(&proposal_list.list, proposal), ENOTVALIDPROPOSAL); 
         let proposal = table::borrow_mut(&mut proposal_list.list, proposal);
@@ -122,7 +122,7 @@ module governance::governance {
         table::add(&mut proposal.voter_list, tx_context::sender(ctx), true);
     }
 
-    public entry fun approve_proposal(proposal_list: &mut ProposalList, proposal: address, ctx: &mut TxContext) {
+    public entry fun approve_proposal(proposal_list: &mut ProposalList, proposal: ID, ctx: &mut TxContext) {
         let og_members = OGMEMBERS;
         assert!(vector::contains(&og_members, &tx_context::sender(ctx)), EINVALIDACCESS);
         let proposal = table::borrow_mut(&mut proposal_list.list, proposal);
@@ -155,12 +155,12 @@ module governance::governance {
             last_voting_time: 0,
             voter_list: table::new(ctx)
         };
-        table::add(&mut user_request_list.list, object::uid_to_address(&new_request.id), new_request);
+        table::add(&mut user_request_list.list, object::uid_to_inner(&new_request.id), new_request);
     }
 
     public entry fun vote_user_request(
         user_request_list: &mut UserRequestList,
-        user_request: address,
+        user_request: ID,
         users: &Users, 
         clock: &Clock, 
         ctx: &mut TxContext
@@ -177,7 +177,7 @@ module governance::governance {
 
     public entry fun decide_user_action(
         user_request_list: &mut UserRequestList,
-        user_request: address,
+        user_request: ID,
         users: &mut Users,
         ctx: &mut TxContext
     ) {
